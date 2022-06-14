adefc108f01b5354d47261bf321cdbaee9dc04d1
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2022-06-14 16:14:16 +0200

    WIP: first working julia field type wrapper

---
+++ apps/common/include/JuliaFieldElement.h
@@ -0,0 +1,456 @@
+/* Copyright (c) 1997-2022
+   Ewgenij Gawrilow, Michael Joswig, and the polymake team
+   Technische Universität Berlin, Germany
+   https://polymake.org
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version: http://www.gnu.org/licenses/gpl.txt.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+--------------------------------------------------------------------------------
+*/
+
+#ifndef POLYMAKE_COMMON_JULIAFIELDELEMENT_H
+#define POLYMAKE_COMMON_JULIAFIELDELEMENT_H
+
+#include "polymake/client.h"
+#include "polymake/Polynomial.h"
+#include "polymake/Integer.h"
+#include "polymake/Array.h"
+
+namespace polymake { namespace common {
+
+namespace juliainterface {
+
+class julia_field_wrap;
+
+}
+
+class JuliaFieldElement;
+
+} }
+
+namespace pm {
+
+template <>
+struct spec_object_traits< polymake::common::JuliaFieldElement>;
+
+}
+
+
+namespace polymake { namespace common {
+
+   // this is currently only for fields that embed the rational numbers
+//template <long index>
+class JuliaFieldElement {
+   private:
+      mutable std::unique_ptr<juliainterface::julia_field_wrap, void(*)(juliainterface::julia_field_wrap*)> impl;
+
+      explicit JuliaFieldElement(juliainterface::julia_field_wrap* x);
+
+   public:
+
+      // constructors
+
+      // 0 in Q
+      JuliaFieldElement();
+
+      ~JuliaFieldElement() = default;
+
+      // x in Q
+      explicit JuliaFieldElement(const Rational& x);
+
+      JuliaFieldElement(const JuliaFieldElement& x);
+      JuliaFieldElement(JuliaFieldElement&& x);
+
+      JuliaFieldElement(void* x, Int index);
+
+      JuliaFieldElement& operator= (const Rational& b);
+      JuliaFieldElement& operator= (const JuliaFieldElement& b);
+
+      // 
+      JuliaFieldElement& operator+= (const Rational& b);
+      JuliaFieldElement& operator-= (const Rational& b);
+      JuliaFieldElement& operator*= (const Rational& b);
+      JuliaFieldElement& operator/= (const Rational& b);
+      JuliaFieldElement& operator+= (const JuliaFieldElement& b);
+      JuliaFieldElement& operator-= (const JuliaFieldElement& b);
+      JuliaFieldElement& operator*= (const JuliaFieldElement& b);
+      JuliaFieldElement& operator/= (const JuliaFieldElement& b);
+
+      JuliaFieldElement& negate();
+
+      friend JuliaFieldElement& negate(JuliaFieldElement& nf) {
+         return nf.negate();
+      }
+
+      friend JuliaFieldElement pow(const JuliaFieldElement& a, Int k);
+
+      Int cmp(const JuliaFieldElement& b) const;
+      Int cmp(const Rational& b) const;
+
+      bool is_zero() const;
+      bool is_one() const;
+      Int is_inf() const;
+      Int sign() const;
+
+      friend JuliaFieldElement abs(const JuliaFieldElement& jf);
+
+      // infinity
+      // sgn: -1 for -inf, 1 for inf and 0 for 0
+      static JuliaFieldElement infinity(Int sgn);
+
+      // conversion
+      explicit operator double() const;
+      explicit operator Rational() const;
+
+
+      // TODO check 
+      inline friend void relocate(JuliaFieldElement* from, JuliaFieldElement* to) {
+         pm::relocate(from,to);
+      }
+
+      inline friend
+      Int isinf(const JuliaFieldElement& a) noexcept
+      {
+         return a.is_inf();
+      }
+      // redirecting:
+
+      // construction from compatible types via Rational
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      explicit JuliaFieldElement(const T& x) : JuliaFieldElement(Rational(x)) {}
+
+      //void set_inf(Int sgn);
+      //void set_inf(const NumberField& nf, Int sgn);
+
+      bool is_finite() const { 
+         return this->is_inf() == 0;
+      }
+
+      // Assignment
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      JuliaFieldElement& operator= (const T& b) 
+      {
+         return *this = Rational(b);
+      }
+      inline friend Int sign(const JuliaFieldElement& jf) {
+         return jf.sign();
+      }
+
+      // Unary Minus
+      inline friend JuliaFieldElement operator- (const JuliaFieldElement& a) {
+         return std::move(JuliaFieldElement(a).negate());
+      }
+
+      friend bool abs_equal(const JuliaFieldElement& jf1, const JuliaFieldElement& jf2);
+
+      // Arithmetic +
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      JuliaFieldElement& operator+= (const T& b)
+      {
+         return *this += Rational(b);
+      }
+
+      inline friend JuliaFieldElement operator+ (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return std::move(JuliaFieldElement(a) += b);
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator+ (const JuliaFieldElement& a, const T& b)
+      {
+         return std::move(JuliaFieldElement(a) += Rational(b));
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator+ (const T& a, const JuliaFieldElement& b)
+      {
+         return b+a;
+      }
+
+      // Arithmetic -
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      JuliaFieldElement& operator-= (const T& b)
+      {
+         return *this -= Rational(b);
+      }
+
+      inline friend JuliaFieldElement operator- (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return std::move(JuliaFieldElement(a) -= b);
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator- (const JuliaFieldElement& a, const T& b)
+      {
+         return std::move(JuliaFieldElement(a) -= Rational(b));
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator- (const T& a, const JuliaFieldElement& b)
+      {
+         return (-b)+a;
+      }
+
+      // Arithmetic *
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      JuliaFieldElement& operator*= (const T& b)
+      {
+         return *this *= Rational(b);
+      }
+
+      inline friend JuliaFieldElement operator* (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return std::move(JuliaFieldElement(a) *= b);
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator* (const JuliaFieldElement& a, const T& b)
+      {
+         return std::move(JuliaFieldElement(a) *= Rational(b));
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator* (const T& a, const JuliaFieldElement& b)
+      {
+         return b*a;
+      }
+
+      // Arithmetic *
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      JuliaFieldElement& operator/= (const T& b)
+      {
+         return *this /= Rational(b);
+      }
+
+      inline friend JuliaFieldElement operator/ (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return std::move(JuliaFieldElement(a) /= b);
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator/ (const JuliaFieldElement& a, const T& b)
+      {
+         return std::move(JuliaFieldElement(a) /= Rational(b));
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      inline friend JuliaFieldElement operator/ (const T& a, const JuliaFieldElement& b)
+      {
+         return std::move(JuliaFieldElement(a) /= b);
+      }
+
+
+      // comparison
+      inline friend bool operator== (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return a.cmp(b) == 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator== (const JuliaFieldElement& a, const T& b)
+      {
+         return a.cmp(Rational(b)) == 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator== (const T& a, const JuliaFieldElement& b)
+      {
+         return b.cmp(a) == 0;
+      }
+
+      // comparison
+      inline friend bool operator!= (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return a.cmp(b) != 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator!= (const JuliaFieldElement& a, const T& b)
+      {
+         return a.cmp(Rational(b)) != 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator!= (const T& a, const JuliaFieldElement& b)
+      {
+         return b.cmp(a) != 0;
+      }
+
+      inline friend bool operator< (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return a.cmp(b) < 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator< (const JuliaFieldElement& a, const T& b)
+      {
+         return a.cmp(Rational(b)) < 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator< (const T& a, const JuliaFieldElement& b)
+      {
+         return b.cmp(a) > 0;
+      }
+
+      inline friend bool operator<= (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return a.cmp(b) <= 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator<= (const JuliaFieldElement& a, const T& b)
+      {
+         return a.cmp(Rational(b)) <= 0;
+      }
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator<= (const T& a, const JuliaFieldElement& b)
+      {
+         return b.cmp(a) >= 0;
+      }
+
+      inline friend bool operator> (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return a.cmp(b) > 0;
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator> (const JuliaFieldElement& a, const T& b)
+      {
+         return a.cmp(Rational(b)) > 0;
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator> (const T& a, const JuliaFieldElement& b)
+      {
+         return b.cmp(a) < 0;
+      }
+
+      inline friend bool operator>= (const JuliaFieldElement& a, const JuliaFieldElement& b)
+      {
+         return a.cmp(b) >= 0;
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator>= (const JuliaFieldElement& a, const T& b)
+      {
+         return a.cmp(Rational(b)) >= 0;
+      }
+
+      template <typename T, typename=std::enable_if_t<pm::can_initialize<pure_type_t<T>, Rational>::value>>
+      friend bool operator>= (const T& a, const JuliaFieldElement& b)
+      {
+         return b.cmp(a) <= 0;
+      }
+
+      std::string to_string() const;
+
+      std::string to_serialized() const;
+
+      static void register_julia_field(void* dispatch_helper, long index);
+
+   }; // end JuliaFieldElement
+
+inline bool abs_equal(const polymake::common::JuliaFieldElement& jf1,const polymake::common::JuliaFieldElement& jf2) {
+   return abs(jf1).cmp(jf2) == 0;
+}
+
+} }
+
+
+namespace pm {
+
+template <>
+struct spec_object_traits< polymake::common::JuliaFieldElement>
+   : spec_object_traits<is_scalar> {
+   typedef polymake::common::JuliaFieldElement persistent_type;
+   typedef void generic_type;
+   typedef is_scalar generic_tag;
+
+   static
+   bool is_zero(const persistent_type& p)
+   {
+      return p.is_zero();
+   }
+
+   static
+   bool is_one(const persistent_type& p)
+   {
+      return p.is_one();
+   }
+
+   static
+   const persistent_type& zero()
+   {
+      static const persistent_type x=persistent_type(0);
+      return x;
+   }
+
+   static
+   const persistent_type& one()
+   {
+      static const persistent_type x(1);
+      return x;
+   }
+};
+
+// convince polymake to properly deserialize this as a composite object even though it is
+// used as a scalar
+template <>
+struct has_serialized<polymake::common::JuliaFieldElement> : std::true_type {};
+/*
+template<>
+   struct spec_object_traits< Serialized < polymake::common::JuliaFieldElementSerializer> >
+   : spec_object_traits<is_composite> {
+      typedef polymake::common::JuliaFieldElementSerializer masquerade_for;
+
+      typedef cons<std::string> elements;
+
+      template <typename Me, typename Visitor>
+         static void visit_elements(Me& me, Visitor& v)
+         {
+            v << std::get<0>(me);
+         }
+   };
+*/
+template <typename Output>
+   Output& operator<< (GenericOutput<Output>& out, const polymake::common::JuliaFieldElement& me) {
+      out.top() << me.to_string();
+      return out.top();
+};
+/*
+template <typename Input>
+Input& operator>> (GenericInput<Input>& is, polymake::common::JuliaFieldElement& jfe)
+{
+   polymake::common::JuliaFieldElementSerializer p;
+   is.top() >> serialize(p);
+   jfe = polymake::common::JuliaFieldElement::from_string(std::get<0>(p));
+   return is.top();
+}
+*/
+template <>
+struct algebraic_traits<polymake::common::JuliaFieldElement> {
+   typedef polymake::common::JuliaFieldElement field_type;
+};
+
+inline
+bool isfinite(const polymake::common::JuliaFieldElement& a) noexcept
+{
+   return a.is_finite();
+}
+
+
+}
+
+namespace std {
+   template <>
+   class numeric_limits<polymake::common::JuliaFieldElement>
+      : public numeric_limits<pm::Rational> {
+   public:
+      static polymake::common::JuliaFieldElement min() { return polymake::common::JuliaFieldElement::infinity(-1); }
+      static polymake::common::JuliaFieldElement max() { return polymake::common::JuliaFieldElement::infinity(1); }
+      static polymake::common::JuliaFieldElement infinity() { return polymake::common::JuliaFieldElement::infinity(1); }
+   };
+}
+
+#endif
---
+++ support/configure.pl
@@ -0,0 +1,60 @@
+# This is a template for a configuration script of a bundled or standalone extension.
+# Please edit it according to your needs and rename it to configure.pl
+# All subroutines defined below must stay here, even if they do nothing.
+
+
+# List of variables used in the ninja rules that must be modified when building this extension.
+# The complete list of recognized variables is contained in the module perllib/Polymake/Configure.pm.
+# Usually you will have to modify only few of them, most probably CXXFLAGS, LDFLAGS, or LIBS.
+# Please put just the bare names of the variables here, without '$' prefix.
+
+@conf_vars=qw( CXXFLAGS LDFLAGS LIBS );
+
+
+# This subroutine should augment the key sets of one or both hash maps passed by reference.
+# These hash maps contain all options recognized by configure script.
+# The first hash map is used for normal options like --docdir.
+# The second hash map is used for trivalent options like --with-java / --without-java.
+#
+# The option disabling this extension completely (only applicable to bundled extensions)
+# is added automatically and should not be mentioned here.
+# Please specify just the bare option names, without '--' prefix.
+
+sub allowed_options {
+   my ($allowed_options, $allowed_with)=@_;
+   @$allowed_with{ qw( julia ) }=();
+}
+
+sub usage {
+   print STDERR "  --with-julia=PATH  installation path of julia, if non-standard\n";
+}
+
+sub proceed {
+   my ($options)=@_;
+   my ($julia_path, $julia_version, $julia_inc, $julia_lib, $julia_config);
+
+   if (defined ($julia_path=$options->{julia})) {
+      $julia_inc="$julia_path/include";
+      $julia_lib=Polymake::Configure::get_libdir($julia_path, "julia");
+   }
+   if ($julia_inc) {
+      if (-f "$julia_inc/julia/julia.h") {
+         $CXXFLAGS = "-I$julia_inc";
+      } else {
+         die "Invalid installation location of julia: header file $julia_inc/julia/julia.h does not exist\n";
+      }
+   }
+   if ($julia_lib) {
+      if (-f "$julia_lib/libjulia.$Config::Config{so}") {
+         $LDFLAGS = "-L$julia_lib";
+         $LDFLAGS .= " -Wl,-rpath,$julia_lib" unless $julia_lib =~ m#^/usr/lib#;
+      } else {
+         die "Invalid installation location of libjulia: library libjulia.$Config::Config{so} does not exist\n";
+      }
+   }
+
+   # TODO: build test program, check version?
+
+   $LIBS="-ljulia";
+   return "$julia_version @ ".($julia_path//"system");
+}
